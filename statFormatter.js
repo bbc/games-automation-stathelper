e0f2877aff673e51102edcebb301654ad7a83282
Alex pb <alex.parry-brown@bbc.co.uk>
2021-06-01 14:52:03 +0100

    Update statFormatter.js

--- statFormatter.js
+++ statFormatter.js
@@ -1,98 +1,98 @@
 export const formatStatConfig = (
-    stat, 
-    { actionName, 
-      actionType, 
-      campaignId, 
-      creationId, 
-      variant, 
-      format, 
-      generalPlacement, 
-      detailedPlacement,  
-      advertiserId, 
-      url, 
-      counterName, 
-      screenName, 
-      bucket, 
-    } = {} ) => {
-    let bucketString;
-    
-    if (actionType && actionName) {
-      stat.creationId = `${actionName}~${actionType}`;
-    }
-
-    if (campaignId) {
-      stat.campaignId = campaignId;
-    }
-
-    if (creationId) {
-      stat.creationId = creationId;
-    }
-
-    if (variant) {
-      stat.variant = variant;
-    }
-
-    if (format) {
-      stat.format = format;
-    }
+  stat, 
+  { actionName, 
+    actionType, 
+    campaignId, 
+    creationId, 
+    variant, 
+    format, 
+    generalPlacement, 
+    detailedPlacement,  
+    advertiserId, 
+    url, 
+    countername, 
+    screenName, 
+    bucket, 
+  } = {} ) => {
+  let bucketString;
   
-    if (generalPlacement) {
-      stat.generalPlacement = generalPlacement;
-    }
+  if (actionType && actionName) {
+    stat.creationId = `${actionName}~${actionType}`;
+  }
 
-    if (detailedPlacement) {
-      stat.detailedPlacement = detailedPlacement;
-    }
+  if (campaignId) {
+    stat.campaignId = campaignId;
+  }
 
-    if (advertiserId) {
-      stat.advertiserId = advertiserId;
-    }
-    
-    if(url) {
-      stat.url = url;
-    }
+  if (creationId) {
+    stat.creationId = creationId;
+  }
 
-    if(counterName) {
-      stat.counterName = counterName;
-    }
+  if (variant) {
+    stat.variant = variant;
+  }
 
-    if (screenName) {
-      stat.screenName = screenName;
-    }
+  if (format) {
+    stat.format = format;
+  }
 
-    if(bucket) {
-      stat.bucket = bucket;
-    }
+  if (generalPlacement) {
+    stat.generalPlacement = generalPlacement;
+  }
+
+  if (detailedPlacement) {
+    stat.detailedPlacement = detailedPlacement;
+  }
+
+  if (advertiserId) {
+    stat.advertiserId = advertiserId;
+  }
   
-    const generalPlacement = async (generalPlacement, screenName) => {
-      if (screenName === "") {
-          return `${generalPlacement}.page`;
-      } else {
-          return `${generalPlacement}.${screenName}.page`;
-      }
-    };
-  
-    const setCountername = async (countername, screenName) => {
-      if (screenName === "") {
-        return `${countername}.page`;
-      } else {
-        return `${countername}.${screen}.page`;
-      }
-    };
-  
-    if (stat.bucket === "testGames") {
-      bucketString = `s=${599452}`;
-    } else if (stat.bucket === "prodGames") {
-      bucketString = `s=${598261}`;
+  if(url) {
+    stat.url = url;
+  }
+
+  if(countername) {
+    stat.countername = countername;
+  }
+
+  if (screenName) {
+    stat.screenName = screenName;
+  }
+
+  if(bucket) {
+    stat.bucket = bucket;
+  }
+
+  const setGeneralPlacement = (generalPlacement, screenName) => {
+    if (screenName === "") {
+        return `${generalPlacement}.page`;
+    } else {
+        return `${generalPlacement}.${screenName}.page`;
     }
-  
-    const counternameString = await setCountername(stat.counterName, stat.screenName);
-    const statString = `[${stat.campaignId}]-[${stat.creationId}]-[${stat.variant}]-[${stat.format}]-[${await generalPlacement(stat.generalPlacement, stat.screenName)}]-[${stat.detailedPlacement}]-[${stat.advertiserId}]-[${stat.url}]`;
-    const newStatData = {
-      bucket: bucketString,
-      stat: statString,
-      counterName: `p=${counternameString}`,
-      event: `echo_event=${stat.event}`,
-    };
-    return newStatData;
   };
+
+  const setCountername = (countername, screenName) => {
+    if (screenName === "") {
+      return `${countername}.page`;
+    } else {
+      return `${countername}.${screenName}.page`;
+    }
+  };
+
+  if (stat.bucket === "testGames") {
+    bucketString = `s=${599452}`;
+  } else if (stat.bucket === "prodGames") {
+    bucketString = `s=${598261}`;
+  }
+
+  const counternameString = setCountername(stat.countername, stat.screenName);
+  const statString = `[${stat.campaignId}]-[${stat.creationId}]-[${stat.variant}]-[${stat.format}]-[${setGeneralPlacement(stat.generalPlacement, stat.screenName)}]-[${stat.detailedPlacement}]-[${stat.advertiserId}]-[${stat.url}]`;
+  const newStatData = {
+    bucket: bucketString,
+    stat: statString,
+    counterName: `p=${counternameString}`,
+    event: `echo_event=${stat.event}`,
+  };
+  return newStatData;
+};
